181f6a8c6c15970b4bfe6153b3413b29f31677f3
rysk1013 <0829ryosuke@gmail.com>
2023-05-21 11:42:49 +0900

    #2-6

--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,5 @@ import { Hero } from '../components/Hero';
 import { Layout } from '../components/Layout';
 
 export default function Home() {
-  return (
-      <Layout>
-        <Hero />
-      </Layout>
-  );
+  return <Hero />;
 }
