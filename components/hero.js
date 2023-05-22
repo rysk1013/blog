e3c64ef51cbd2adea56e9bd956723b704698510a
rysk1013 <0829ryosuke@gmail.com>
2023-05-22 22:58:16 +0900

    #3-5

--- components/hero.js
+++ components/hero.js
@@ -1,8 +1,12 @@
+import styles from '../styles/hero.module.css'
+
 export const Hero = (props) => {
   return (
     <div>
-      <h1>{ props.title }</h1>
-      <p>{props.subtitle}</p>
+      <div className={styles.text}>
+        <h1 className={styles.title}>{props.title}</h1>
+        <p className={styles.subtitle}>{props.subtitle}</p>
+      </div>
       {props.imageOn && <figure> [画像] </figure>}
     </div>
   );
