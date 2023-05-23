f2e21122d38347845fea07d1a99d45e15ea5cbc6
rysk1013 <0829ryosuke@gmail.com>
2023-05-23 23:55:13 +0900

    #4-2

--- components/header.js
+++ components/header.js
@@ -1,11 +1,14 @@
 import { Logo } from 'components/Logo';
 import { Nav } from 'components/Nav';
+import styles from 'styles/header.module.css';
 
 export const Header = () => {
   return (
     <header>
-      <Logo boxOn />
-      <Nav />
+      <div className={styles.flexContainer}>
+        <Logo boxOn />
+        <Nav />
+      </div>
     </header>
   );
 }
