2a80ff8112c53b45ac49479f0e14b4fe41bc70ec
rysk1013 <0829ryosuke@gmail.com>
2023-05-24 22:08:36 +0900

    #4-3

--- components/hero.js
+++ components/hero.js
@@ -2,7 +2,7 @@ import styles from 'styles/hero.module.css'
 
 export const Hero = (props) => {
   return (
-    <div>
+    <div className={styles.flexContainer}>
       <div className={styles.text}>
         <h1 className={styles.title}>{props.title}</h1>
         <p className={styles.subtitle}>{props.subtitle}</p>
