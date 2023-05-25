ca9a6eeeaaf63c53b4b865577b026682d40415f3
rysk1013 <0829ryosuke@gmail.com>
2023-05-25 22:39:30 +0900

    #4-8

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,7 @@
 import { Container } from 'components/Container';
 import { Hero } from 'components/Hero';
 import { PostBody } from 'components/Post-body';
+import { Contact } from 'components/Contact';
 
 export default function About() {
   return (
@@ -9,6 +10,7 @@ export default function About() {
         title="About"
         subtitle="About development activities"
       />
+
       <PostBody>
         <p>
           Cubeが得意とする分野はモノづくりです。3次元から2次元の造形、プログラミングやデザインなど、さまざまな技術を組み合わせることによって社会や環境と結びつけるクリエイティブを提案し続けています。
@@ -25,6 +27,8 @@ export default function About() {
           今までと違うものを作ることで愛着が湧いてきます。そこで興味を持ったことは小さなことでもいいから取り入れて、良いものを作れるようにしています。小さなヒントから新しいものを生み出すようなモノづくりは、これからも続けていきたいです。
         </p>
       </PostBody>
+
+      <Contact />
     </Container>
   );
 }
