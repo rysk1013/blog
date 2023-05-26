74139abae91e4d4f93b3359a80590bd0c03cf23b
rysk1013 <0829ryosuke@gmail.com>
2023-05-26 22:03:18 +0900

    #5-5

--- pages/about.js
+++ pages/about.js
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+import eyecatch from 'images/about.jpg';
 import { Container } from 'components/Container';
 import { Hero } from 'components/Hero';
 import { PostBody } from 'components/Post-body';
@@ -11,6 +13,17 @@ export default function About() {
         title="About"
         subtitle="About development activities"
       />
+
+      <figure>
+        <Image
+          src={eyecatch}
+          alt=""
+          layout="responsive"
+          sizes="(min-width: 1152px) 1152px, 100vw"
+          priority
+          placeholder="blur"
+        />
+      </figure>
 
       <PostBody>
         <TwoColumn>
